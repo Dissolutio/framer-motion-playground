9b77d336fbda372d9e784a49f1913ca4d7bbba21
John Moen <entity.john@gmail.com>
2023-01-04 15:53:39 -0600

    whole bunch of hex map sizing viewbox progress

--- src/hexgrid/HexText.tsx
+++ src/hexgrid/HexText.tsx
@@ -1,31 +1,30 @@
-import * as React from 'react'
-import styled from 'styled-components'
+import * as React from "react";
+import styled from "styled-components";
 
 export type Props = {
-  children: string | React.ReactNode | React.ReactNode[]
-  hexSize: number
-  x?: string | number
-  y?: string | number
-  className?: string
-} & React.SVGProps<SVGTextElement>
+  children: string | React.ReactNode | React.ReactNode[];
+  hexSize: number;
+  x?: string | number;
+  y?: string | number;
+  className?: string;
+} & React.SVGProps<SVGTextElement>;
 
 export function HexText(props: Props) {
-  const { children, hexSize, x, y } = props
+  const { children, hexSize, x, y } = props;
   return (
     <StyledText
       hexSize={hexSize}
       x={x || 0}
-      y={y ? y : '0.3em'}
+      y={y ? y : "0.3em"}
       textAnchor="middle"
     >
       {children}
     </StyledText>
-  )
+  );
 }
 type StyledTextProps = React.SVGProps<SVGTextElement> & {
-  hexSize: number
-}
+  hexSize: number;
+};
 const StyledText = styled.text<StyledTextProps>`
-  fill: var(--sub-white);
   font-size: ${(props) => `${props.hexSize / 75}rem`};
-`
+`;
--- src/hexgrid/Hexgrid.tsx
+++ src/hexgrid/Hexgrid.tsx
@@ -2,33 +2,44 @@ import React from "react";
 import { HexgridLayoutProvider } from "./HexgridLayout";
 import Hexagon from "./Hexagon";
 import "./hexgrid-styles.css";
-import { useWindowSize } from "../useWindowSize";
 import { HexCoordinates } from "./types";
 import { HexText } from "./HexText";
 import { hexUtilsSubtract } from "./hex-utils";
-import { generateRectangleHexgrid } from "./hex-utils copy";
+import {
+  generateHexagonHexgrid,
+  generateRectangleHexgrid,
+} from "./hex-utils copy";
 import styled from "styled-components";
 
 export const Hexgrid = () => {
-  const size = useWindowSize();
-  console.log("🚀 ~ file: Hexgrid.tsx:11 ~ Hexgrid ~ size", size);
-  const shiftMapToOrigin = (hexes: HexCoordinates[]): HexCoordinates[] => {
-    const northWestHex = hexes[0];
-    const newHexes = hexes.map((hex) => {
-      return hexUtilsSubtract(hex, northWestHex);
-    });
-    return newHexes;
-  };
-  // const hexagons = shiftMapToOrigin(GridGenerator.parallelogram(0, 2, 0, 2));
-  const hexagons = generateRectangleHexgrid(1, 1);
+  // const size = useWindowSize();
   const hexagonSize = 10;
-  // const hexagons = shiftMapToOrigin(GridGenerator.hexagon(1));
+  const mapSize = 10;
+  const mapHeight = 17;
+  const mapWidth = 33;
+
+  const hexagons = generateRectangleHexgrid(mapWidth, mapHeight);
+  const viewbox = calculateViewbox(
+    "rectangle",
+    mapSize,
+    mapHeight,
+    mapWidth,
+    hexagonSize
+  );
+
+  // const hexagons = shiftMapToOrigin(generateHexagonHexgrid(mapSize));
+  // const viewbox = calculateViewbox("shiftedHexagon", mapSize, hexagonSize);
+
+  // const hexagons = generateHexagonHexgrid(mapSize);
+  // const viewbox = calculateViewbox("unshiftedHexagon", mapSize, hexagonSize);
+
   return (
     <StyledDiv>
       <svg
-        width={"100vw"}
-        height={"100vw"}
-        viewBox={`-50 -50 100 100`}
+        width={"100%"}
+        height={"100%"}
+        viewBox={viewbox}
+        style={{ background: "indianred" }}
         version="1.1"
         xmlns="http://www.w3.org/2000/svg"
       >
@@ -54,4 +65,60 @@ export const Hexgrid = () => {
     </StyledDiv>
   );
 };
-const StyledDiv = styled.div``;
+
+const StyledDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  max-height: 100vh;
+`;
+
+const shiftMapToOrigin = (hexes: HexCoordinates[]): HexCoordinates[] => {
+  const northWestHex = hexes[0];
+  const newHexes = hexes.map((hex) => {
+    return hexUtilsSubtract(hex, northWestHex);
+  });
+  return newHexes;
+};
+const calculateViewbox = (
+  mapShape: string,
+  mapSize: number,
+  mapHeight: number,
+  mapWidth: number,
+  hexSize: number
+) => {
+  const a = (hexSize * Math.sqrt(3)) / 2; // apothem
+  let height: number = 100;
+  let yShift: number = -50;
+  let width: number = 100;
+  let xShift: number = -50;
+  switch (mapShape) {
+    case "rectangle":
+      height = 2 * hexSize + (mapHeight - 1) * 1.5 * hexSize; // 2r+(n-1)1.5r
+      yShift = -1 * hexSize; // -r
+      width = 2 * a * mapWidth + (mapHeight > 1 ? a : 0); // 2an + odd-row shift (a second row of hexes will be shifted further right than the first row)
+      xShift = -1 * a; // -a
+      return toStringViewBox(xShift, yShift, width, height);
+    case "unshiftedHexagon":
+      height = 2 * (hexSize + 1.5 * (mapSize * hexSize)); // 2(r + 1.5nr)
+      yShift = (-1 * height) / 2;
+      width = 2 * a * (2 * mapSize + 1); // 2a(2n+1)
+      xShift = -1 * a * (2 * mapSize + 1); // -a(1+2n)
+      return toStringViewBox(xShift, yShift, width, height);
+    case "shiftedHexagon":
+      height = 2 * (hexSize + 1.5 * (mapSize * hexSize)); // 2(r + 1.5nr)
+      yShift = (-1 * height) / 2;
+      width = 2 * a * (2 * mapSize + 1); // 2a(2n+1)
+      xShift = -1 * a; // -a
+      return toStringViewBox(xShift, yShift, width, height);
+    default:
+      return toStringViewBox(xShift, yShift, width, height);
+  }
+};
+const toStringViewBox = (
+  x: number,
+  y: number,
+  w: number,
+  h: number
+): string => {
+  return `${x} ${y} ${w} ${h}`;
+};
